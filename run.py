4a11cfb4f7c99bb943f3a28f7bbd13531aaff79a
Cormac Hogan <cormachogan1916@gmail.com>
2021-11-19 23:22:53 +0000

    add a or b and yes or no functions and opening questions to main for decisions on what part of the program to run

--- run.py
+++ run.py
@@ -5,7 +5,34 @@
 
 
 
+# Function for a Yes/No result based on the answer provided as an arguement
+def a_or_b(answer,a,b):
+    while True:
+        choice = input(answer).lower()
+        if choice in a:
+           return True
+        elif choice in b:
+           return False
+        else:
+           print(f"Please respond with {a[0]} or '{b[0]}'\n")
+
+
+def yes_no(answer):
+    yes = set(['yes','y', 'ye'])
+    no = set(['no','n'])
+    return a_or_b(answer,yes,no)
+
+
+     
+
+
+def main():
+    print("Welcome to fitness_calculator v3. this calculator can be used for bmi,calories,macros and fibre using industry standard calculations.\n")
+    bmi_bool = yes_no("would you like to calculate your BMI and healthy weight range? y/n: \n")
+    calorie_bool = yes_no("would you like to calculate your calories? y/n: \n")
+    macro_bool = yes_no("would you like to calculate your macros? y/n: \n")
+    units = a_or_b("would you like to use lb or kg for measurements? type lb or kg: \n","lb","kg" )
 
 
 
-main():
+main()
