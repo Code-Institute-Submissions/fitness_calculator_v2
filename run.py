d7982f9b6f1742fcdffff57406ab18107e165e53
Cormac Hogan <cormachogan1916@gmail.com>
2021-11-20 20:30:10 +0000

    tidy up formatting to reduce detected problems

--- run.py
+++ run.py
@@ -1,29 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-
-
 
 # Function for a Yes/No result based on the answer provided as an arguement
-def a_or_b(answer,a,b):
+def a_or_b(answer, a, b):
     while True:
         choice = input(answer).lower()
         if choice in a:
-           return True
+            return True
         elif choice in b:
-           return False
+            return False
         else:
-           print(f"Please respond with {a[0]} or '{b[0]}'\n")
+            print(f"Please respond with {a[0]} or '{b[0]}'\n")
 
 
 def yes_no(answer):
-    yes = set(['yes','y', 'ye'])
-    no = set(['no','n'])
-    return a_or_b(answer,yes,no)
-
-
-     
+    yes = set(['yes', 'y', 'ye'])
+    no = set(['no', 'n'])
+    return a_or_b(answer, yes, no)
 
 
 def main():
@@ -33,11 +24,10 @@ def main():
     calorie_bool = yes_no("would you like to calculate your calories? y/n: \n")
     macro_bool = yes_no("would you like to calculate your macros? y/n: \n")
 
-    if bmi_bool && calorie_bool && macro_bool = False:
+    if bmi_bool and calorie_bool and macro_bool is False:
         # end_program()
 
-    units = a_or_b("would you like to use lb or kg for measurements? type lb or kg: \n","lb","kg" ) 
-
+    units = a_or_b("would you like to use lb or kg for measurements? type lb or kg: \n", "lb", "kg")
 
 
 main()
