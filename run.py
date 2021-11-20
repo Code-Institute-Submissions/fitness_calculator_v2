bfb0c3a631a81ce3ddd7207b36734670137e1201
Cormac Hogan <cormachogan1916@gmail.com>
2021-11-20 21:36:00 +0000

    abstracted a_or_b function for easier reuse and formatted document with pysense

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
 
-# Function for a 2 option selection 
+# Function for a 2 option selection
 def a_or_b(answer, a, b):
     while True:
-        choice = input(answer).lower()
+        choice = input(f"{answer} {a[0]}/{b[0]}: \n").lower()
         if choice in a:
             return True
         elif choice in b:
             return False
         else:
-            print(f"Please respond with {a[0]} or '{b[0]}'\n")
+            print(f"Please respond with {a[0]} or {b[0]}\n")
 
 
 def yes_no(answer):
     yes = ['yes', 'y', 'ye']
-    no =  ['no', 'n']
+    no = ['no', 'n']
     return a_or_b(answer, yes, no)
+
 
 def get_value(answer, x, y):
     while True:
@@ -23,24 +24,26 @@ def get_value(answer, x, y):
             return value_answer
         except ValueError:
             print("incorrect input. please try again")
-            
 
 
 # def bmi_calculation_start():
-    
+
 
 def main():
     print("Welcome to fitness_calculator v3. this calculator can be used for bmi,calories,macros and fibre using industry standard calculations.\n")
 
-    bmi_bool = yes_no("would you like to calculate your BMI and healthy weight range? y/n: \n")
-    calorie_bool = yes_no("would you like to calculate your calories? y/n: \n")
-    macro_bool = yes_no("would you like to calculate your macros? y/n: \n")
+    bmi_bool = yes_no(
+        "would you like to calculate your BMI and healthy weight range?")
+    calorie_bool = yes_no("would you like to calculate your calories?")
+    macro_bool = yes_no("would you like to calculate your macros?")
 
-    if bmi_bool and calorie_bool and macro_bool is False:
-        # end_program()
+    # if bmi_bool and calorie_bool and macro_bool is False:
+    #     # end_program()
 
-    weight_units = a_or_b("would you like to use lb or kg for weight measurements? type lb or kg: \n", "lb", "kg")
-    height_units = a_or_b("would you like to use lb or kg for height measurements? type ft or cm: \n", "ft", "cm")
+    weight_units = a_or_b("would you like to use lb or kg for weight measurements?", [
+                          "lb", "l"], ["kg", "k"])
+    height_units = a_or_b("would you like to use lb or kg for height measurements?", [
+                          "ft", "f"], ["cm", "c"])
 
 
 main()
