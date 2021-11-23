235666fe692efd8b012705665626924086a209ea
Cormac Hogan <cormachogan1916@gmail.com>
2021-11-23 20:32:23 +0000

    fix get value function to correctly return value. add questions to bmi calculation start function

--- run.py
+++ run.py
@@ -17,19 +17,36 @@ def yes_no(answer):
     return a_or_b(answer, yes, no)
 
 
-def get_value(answer, low, high, unit):
-    while True:
-        try:
-            value_answer = int(input(f"{answer} in {unit}?\n") in range(low, high))
-            return value_answer
-        except ValueError:
-            print("incorrect input. please try again")
+def get_value(answer, low, high):
+    input_str = input(f"{answer}\n")
+    input_int = int(input_str)
+    if input_int in range(low, high):
+        return input_int
+    else:
+        print(
+            f"invalid answer. please respond with a integer in the range {low} to {high}")
 
 
 def bmi_calculation_start():
     print("This BMI calculator will ask you a few simple questions and return your BMI and healthy weight range")
+
     age = get_value("what is your age?: ", 18, 140)
-    height = get_value("what is your height",)
+
+    if (height_units):
+        height_in_ft = get_value(
+            "(you will be asked to add inches in the next question) what is your height in ft?: ", 1, 9)
+        height_in_inches = get_value(
+            "add inches to your current height: ", 0, 12)
+    else:
+        height_in_cm = get_value(
+            "what is your height in cm: ", 1, 9)
+
+    if (weight_units):
+        weight_in_lb = get_value(
+            "what is your weight in lb?: ", 40, 600)
+    else:
+        weight_in_kg = get_value(
+            "what is your weight in kg?: ", 25, 300)
 
 
 def main():
@@ -40,12 +57,11 @@ def main():
     calorie_bool = yes_no("would you like to calculate your calories?")
     macro_bool = yes_no("would you like to calculate your macros?")
 
-
-
     weight_units = a_or_b("would you like to use lb or kg for weight measurements?", [
                           "lb", "l"], ["kg", "k"])
-    height_units = a_or_b("would you like to use lb or kg for height measurements?", [
+    height_units = a_or_b("would you like to use ft or cm for height measurements?", [
                           "ft", "f"], ["cm", "c"])
+
     # if (bmi_bool):
     #     bmi_calculation_start()
     # elif (calorie_bool):
@@ -54,6 +70,6 @@ def main():
     #     macro_calculation_start()
     # else:
     #     end_program()
-        
+
 
 main()
